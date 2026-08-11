0010e933d9b2e87120b86031ca9f4775081981a9
tilakdubey41-a11y <tilakdubey41@gmail.com>
2026-08-11 04:35:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -106,4 +106,19 @@ document.addEventListener("mousemove", event => {
     earth.style.transform =
         `rotateY(${x}deg) rotateX(${-y}deg)`;
 
+   const earth = document.querySelector(".earth");
+
+document.addEventListener("mousemove", (event) => {
+
+    const x =
+        (event.clientX / window.innerWidth - 0.5) * 8;
+
+    const y =
+        (event.clientY / window.innerHeight - 0.5) * 6;
+
+    earth.style.transform =
+        `rotateY(${x}deg) rotateX(${-y}deg)`;
+
+});
+
 });
